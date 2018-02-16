66b71c49b6fa4d1770e169ebdc4ca06385d6256e
Esraa1moshsen <36335832+Esraa1moshsen@users.noreply.github.com>
2018-02-16 22:01:22 +0200

    first step untested

---
+++ CONTROL_PROJECT.ino
@@ -0,0 +1,62 @@
+
+char data = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode (13,OUTPUT);
+pinMode (12,OUTPUT);
+pinMode (11,OUTPUT);
+pinMode (10,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (Serial.available() >0)
+{
+  data =Serial.read();
+  if (data == '1')
+  {
+     digitalWrite (13,HIGH);
+     digitalWrite (12,LOW);
+     digitalWrite (11,HIGH);
+     digitalWrite (10,LOW);
+     
+  }
+  else if (data == '2')
+  {
+    digitalWrite (12,HIGH);
+     digitalWrite (13,LOW);
+     digitalWrite (10,HIGH);
+     digitalWrite (11,LOW);
+     
+  }
+   else if (data == '3')
+  {
+    digitalWrite (12,HIGH);
+     digitalWrite (13,LOW);
+     digitalWrite (10,LOW);
+     digitalWrite (11,HIGH);
+     
+     
+  }
+  else if (data == '4')
+  {
+    digitalWrite (12,LOW);
+     digitalWrite (13,HIGH);
+     digitalWrite (10,HIGH);
+     digitalWrite (11,LOW);
+     
+     
+  }
+  else 
+  {
+     digitalWrite (12,LOW);
+     digitalWrite (13,LOW);
+     digitalWrite (10,LOW);
+     digitalWrite (11,LOW);
+     
+  }
+}
+}
