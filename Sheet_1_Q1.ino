905bf61aaf28034e416155a1aa68b031125c6ce6
AsmaaMohamed12 <36335920+AsmaaMohamed12@users.noreply.github.com>
2018-02-19 19:48:18 +0200

    Add files via upload

---
+++ Sheet_1_Q1.ino
@@ -0,0 +1,16 @@
+// 1 LED
+
+ int led = 4 ;
+ void setup() {
+
+  pinMode(4,OUTPUT);
+
+  }
+  void loop() {
+    digitalWrite(4,HIGH);
+    delay(500);
+    digitalWrite(4,LOW);
+    delay(1500);
+  }
+
+
