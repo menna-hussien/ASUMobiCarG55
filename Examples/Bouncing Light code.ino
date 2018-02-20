bbfd7ff8630717dd3c3bf37343f0ce182ed7fbd7
Esraa1moshsen <36335832+Esraa1moshsen@users.noreply.github.com>
2018-02-20 20:40:53 +0200

    Add files via upload

---
+++ Examples/Bouncing Light code.ino
@@ -0,0 +1,40 @@
+
+int ledRed = 4;
+int ledYellow =5;
+int ledGreen = 6;
+int ledBlue = 7 ;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (ledRed, OUTPUT);
+pinMode (ledYellow, OUTPUT);
+pinMode (ledGreen, OUTPUT);
+pinMode (ledBlue, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (ledRed,HIGH);
+delay (1000);
+digitalWrite (ledRed,LOW);
+digitalWrite (ledYellow,HIGH);
+delay (1000);
+digitalWrite (ledYellow,LOW);
+digitalWrite (ledGreen,HIGH);
+delay (1000);
+digitalWrite (ledGreen,LOW);
+digitalWrite (ledBlue,HIGH);
+delay (2000);
+digitalWrite (ledBlue,LOW);
+
+digitalWrite (ledGreen,HIGH);
+delay (1000);
+digitalWrite (ledGreen,LOW);
+digitalWrite (ledYellow,HIGH);
+delay (1000);
+digitalWrite (ledYellow,LOW);
+digitalWrite (ledRed,HIGH);
+delay (1000);
+digitalWrite (ledRed,LOW);
+
+}
---
+++ Examples/sketch_jul12a.ino
@@ -0,0 +1,124 @@
+  void setup() {
+
+pinMode (0,INPUT);
+pinMode (1,INPUT);
+pinMode (2,INPUT);
+pinMode (3,INPUT);
+pinMode (4,INPUT);
+pinMode (5,INPUT);
+pinMode (6,INPUT);
+pinMode (13,OUTPUT);
+pinMode (12,OUTPUT);
+pinMode (11,OUTPUT);
+pinMode (10,OUTPUT);
+pinMode (9,OUTPUT);
+pinMode (8,OUTPUT);
+pinMode (7,OUTPUT);
+
+
+}
+
+void loop() {
+
+
+if ( digitalRead (0)==0 &&     digitalRead (1)==1 && digitalRead (2)==1 && digitalRead (3)==1 && digitalRead (4)==1 && digitalRead (5)==1 && digitalRead (6)==1       )
+{
+digitalWrite (13,HIGH);
+digitalWrite (12,HIGH);
+digitalWrite (11,HIGH);
+digitalWrite (10,HIGH);
+digitalWrite (9,HIGH);
+digitalWrite (8,HIGH);
+digitalWrite (7,LOW);
+
+}
+else if (digitalRead (0)==1 &&     digitalRead (1)==0 && digitalRead (2)==1 && digitalRead (3)==1 && digitalRead (4)==1 && digitalRead (5)==1 && digitalRead (6)==1  )
+{
+
+digitalWrite (13,LOW);
+digitalWrite (12,HIGH);
+digitalWrite (11,HIGH);
+digitalWrite (10,LOW);
+digitalWrite (9,LOW);
+digitalWrite (8,HIGH);
+digitalWrite (7,HIGH);
+  
+}
+else if (digitalRead (0)==1 &&     digitalRead (1)==1 && digitalRead (2)==0 && digitalRead (3)==1 && digitalRead (4)==1 && digitalRead (5)==1 && digitalRead (6)==1)
+{
+
+digitalWrite (13,HIGH);
+digitalWrite (12,LOW);
+digitalWrite (11,HIGH);
+digitalWrite (10,HIGH);
+digitalWrite (9,LOW);
+digitalWrite (8,HIGH);
+digitalWrite (7,HIGH);
+  
+}
+else if (digitalRead (0)==1 &&     digitalRead (1)==1 && digitalRead (2)==1 && digitalRead (3)==0 && digitalRead (4)==1 && digitalRead (5)==1 && digitalRead (6)==1)
+{
+
+  digitalWrite (13,HIGH);
+digitalWrite (12,LOW);
+digitalWrite (11,HIGH);
+digitalWrite (10,HIGH);
+digitalWrite (9,HIGH);
+digitalWrite (8,HIGH);
+digitalWrite (7,HIGH);
+}
+else if (digitalRead (0)==1 &&     digitalRead (1)==1 && digitalRead (2)==1 && digitalRead (3)==1 && digitalRead (4)==0 && digitalRead (5)==1 && digitalRead (6)==1)
+{
+
+  digitalWrite (13,LOW);
+digitalWrite (12,HIGH);
+digitalWrite (11,HIGH);
+digitalWrite (10,LOW);
+digitalWrite (9,LOW);
+digitalWrite (8,LOW);
+digitalWrite (7,LOW);
+}
+else if (digitalRead (0)==1 &&     digitalRead (1)==1 && digitalRead (2)==1 && digitalRead (3)==1 && digitalRead (4)==1 && digitalRead (5)==0 && digitalRead (6)==1)
+{
+  digitalWrite (13,HIGH);
+digitalWrite (12,HIGH);
+digitalWrite (11,LOW);
+digitalWrite (10,HIGH);
+digitalWrite (9,HIGH);
+digitalWrite (8,LOW);
+digitalWrite (7,HIGH);
+}
+else if (digitalRead (0)==1 &&     digitalRead (1)==1 && digitalRead (2)==1 && digitalRead (3)==1 && digitalRead (4)==1 && digitalRead (5)==1 && digitalRead (6)==0)
+{
+
+  digitalWrite (13,HIGH);
+digitalWrite (12,HIGH);
+digitalWrite (11,HIGH);
+digitalWrite (10,HIGH);
+digitalWrite (9,LOW);
+digitalWrite (8,LOW);
+digitalWrite (7,HIGH);
+}
+else {
+
+  digitalWrite (13,HIGH);
+digitalWrite (12,HIGH);
+digitalWrite (11,HIGH);
+digitalWrite (10,HIGH);
+digitalWrite (9,HIGH);
+digitalWrite (8,HIGH);
+digitalWrite (7,HIGH);
+}
+
+
+
+
+
+
+
+
+
+  
+ 
+
+}
