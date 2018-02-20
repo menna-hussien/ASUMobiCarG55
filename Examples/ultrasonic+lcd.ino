3d2e7e4a62816e02ba93775347674e6cd6230fbc
monaa12 <36414725+monaa12@users.noreply.github.com>
2018-02-20 21:15:03 +0200

    Add files via upload

---
+++ Examples/ultrasonic+lcd.ino
@@ -0,0 +1,23 @@
+#include <NewPing.h>
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(11,10,9,8,7,6);
+const int trigger=13;
+const int echo=12;
+float t,d;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (trigger,OUTPUT);
+  pinMode (echo,INPUT);
+  lcd.begin(16,2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,0);
+delayMicroseconds(2);
+digitalWrite (13,1);
+t= pulseIn(echo,HIGH);
+d=t/28;
+lcd.setCursor(0,0);
+lcd.print(String(d));
+}
