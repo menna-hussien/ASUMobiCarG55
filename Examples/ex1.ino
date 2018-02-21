2bc046e54e347119cda974d652479712d3f6f74e
MonaEltokhy <36299087+MonaEltokhy@users.noreply.github.com>
2018-02-21 23:48:43 +0200

    Add files via upload

---
+++ Examples/ex1.ino
@@ -0,0 +1,17 @@
+int LED1 =4;
+
+
+void setup() {
+  
+  
+  pinMode(LED1 , OUTPUT);
+ 
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED1 , HIGH); 
+  delay(500);                       
+  digitalWrite(LED1, LOW);    
+  delay(1500); 
+}
