dd3259d4c3df25c1f9cd619fae16de08b2936fc7
HaidySamir1696 <36296063+HaidySamir1696@users.noreply.github.com>
2018-02-21 23:31:02 +0200

    Q1 sheet1 Examples

---
+++ Examples/_111.ino
@@ -0,0 +1,15 @@
+int LED1=11;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED1, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(LED1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1300);                       // wait for a second
+}
