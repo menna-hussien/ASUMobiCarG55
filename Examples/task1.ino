c2af7c4133c2cc6c3d122e37bbb7ecfc86b556a7
nourelhoda25 <36296119+nourelhoda25@users.noreply.github.com>
2018-02-22 19:57:10 +0200

    Add files via upload

---
+++ Examples/task1.ino
@@ -0,0 +1,19 @@
+const int data = 13 ;
+const int latch = 12;
+const int clck = 11 ;
+byte led []={B00000110,B01011011,B01001111,B01100110,B01101101,B01111101,B01000111};
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(data ,OUTPUT );
+pinMode(latch ,OUTPUT );
+pinMode(clck ,OUTPUT );
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i =0 ; i < sizeof(led) ; i++ )
+{digitalWrite(latch,0);
+shiftOut(13,11,MSBFIRST,led [i]);
+digitalWrite(latch,1);
+delay(1000);
+}}
