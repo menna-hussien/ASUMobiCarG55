8ec4d5e7848c15ab4bc9c6686e398179b9593d33
nourelhoda25 <36296119+nourelhoda25@users.noreply.github.com>
2018-03-29 19:47:23 +0200

    Update directions+ultrasonic.ino

--- ultrasonic/directions+ultrasonic.ino
+++ ultrasonic/directions+ultrasonic.ino
@@ -1,3 +1,7 @@
+#include<NewPing.h>
+const int trigger=5;
+const int echo=2;
+NewPing ultrasonic(trigger,echo);
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -7,12 +11,40 @@ pinMode(11,OUTPUT);
 pinMode(10,OUTPUT);
 pinMode(9,OUTPUT);
 pinMode(6,OUTPUT);
+   pinMode(5,OUTPUT);
+  pinMode(2,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(9,HIGH);
   digitalWrite(6,HIGH);
+  digitalWrite(5,0);
+delayMicroseconds(2);
+digitalWrite(5,1);
+delayMicroseconds(10);
+digitalWrite(5,0);
+ float d=ultrasonic.ping_cm();
+   if (d<=30)
+{ 
+ right();
+ delay(1000);
+ forward();
+ d=ultrasonic.ping_cm();
+  if(d<=30)
+  {
+ right();
+ delay(1000);
+ 
+ forward();
+}
+  else {
+  forward();
+}
+}
+else {
+   forward();
+}
 if(Serial.available()>0){
   char r=Serial.read();
   switch (r) {
@@ -48,4 +80,17 @@ if(Serial.available()>0){
   }
 }
 }
-
+void forward()
+{
+   digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH); 
+  digitalWrite(10,LOW);
+}
+void right()
+{
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW); 
+  digitalWrite(10,HIGH); 
+}
