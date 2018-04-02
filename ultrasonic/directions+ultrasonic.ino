027f73e0fc22a271726db98697db363f8524a49b
monaa12 <36414725+monaa12@users.noreply.github.com>
2018-04-02 19:14:22 +0200

    Update directions+ultrasonic.ino

--- ultrasonic/directions+ultrasonic.ino
+++ ultrasonic/directions+ultrasonic.ino
@@ -1,117 +1,137 @@
 #include<NewPing.h>
-const int trigger=5;
-const int echo=2;
+const int trigger = 5;
+const int echo = 2;
+NewPing ultrasonic(trigger, echo);
+char data = 0;
 int motor1f = 13;
 int motor1b = 12;
 int motor2f = 11;
 int motor2b = 10;
 int spd1 = 9;
 int spd2 = 6;
-long duration,d ;
-NewPing ultrasonic(trigger,echo);
+long duration;
+long distance;
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-  pinMode(moror1f,OUTPUT);
-  pinMode(motor1b,OUTPUT);
-  pinMode(motor2f,OUTPUT);
-  pinMode(motor2b,OUTPUT);
-  pinMode(spd1,OUTPUT);
-pinMode(spd2,OUTPUT);
-   pinMode(5,OUTPUT);
-  pinMode(2,INPUT);
+  Serial.begin(9600);
+  pinMode (motor1f, OUTPUT);
+  pinMode (motor1b, OUTPUT);
+  pinMode (motor2f, OUTPUT);
+  pinMode (motor2b, OUTPUT);
+  pinMode (spd1, OUTPUT);
+  pinMode (spd2, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()>0)
-  { r=Serial.read();
-   usReading();
-   if (d<=30)
-{  Serial.println("d<30");
- right();
- delay(300);
- stop();
- delay(1000);
-  else {
-  forward();
-}
-}
- if (USReadings() > 30) {
+  if (Serial.available() > 0)
+  {
+
+    data = Serial.read();
+    if (USReadings() > 30) {
       Serial.println("USReadings()>30");
-  switch (r) {
-    case 'F' : //forward
-     forward();
-    break;
-    case 'B' : //backward
-    backward();
-    break;
-    case 'R' : //right
-   right();
-    break;
-    case 'L' : //left
-    left();
-    break;
-    default: //stop
-   stop();
+      switch (data)
+      {
+        case 'F' :
+        Serial.println("forward");
+          forward();
+          data = 0;
+          break;
+
+        case 'B' :
+          backward();
+          data = 0;
+          break;
+
+        case 'R' :
+          left();
+          data = 0;
+
+          break;
+        case 'L' :
+          right();
+          data = 0;
+
+          break;
+
+        default:
+          digitalWrite (motor1b, LOW);
+          digitalWrite (motor1f, LOW);
+          digitalWrite (motor2b, LOW);
+          digitalWrite (motor2f, LOW);
+          digitalWrite(spd1, HIGH);
+          digitalWrite(spd2, HIGH);
+          //  analogWrite (9,255);
+          //analogWrite (6,255);
+
+      }
+    }
+    else if (USReadings() < 30)
+    { Serial.println("d<30");
+      right ();
+    delay(300);
+      STOP();
+      delay(1000);
+      forward();
+    } else STOP();
   }
- }
-}
 }
 void forward()
 {
-   digitalWrite(motor1f,HIGH);
-  digitalWrite(motor1b,LOW);
-  digitalWrite(motor2f,HIGH); 
-  digitalWrite(motor2b,LOW);
-   digitalWrite(spd1,HIGH);
-  digitalWrite(spd2,HIGH);
+  digitalWrite(motor1f, HIGH);
+  digitalWrite(motor1b, LOW);
+  digitalWrite(motor2f, HIGH);
+  digitalWrite(motor2b, LOW);
+  digitalWrite(spd1, HIGH);
+  digitalWrite(spd2, HIGH);
 }
+
 void right()
 {
-  
-   digitalWrite(motor1f,LOW);
-  digitalWrite(motor1b,HIGH);
-  digitalWrite(motor2f,HIGH);
-  digitalWrite(motor2b,LOW);
-   digitalWrite(spd1,HIGH);
-  digitalWrite(spd2,HIGH);
+  digitalWrite(motor1f, HIGH);
+  digitalWrite(motor1b, LOW);
+  digitalWrite(motor2f, LOW);
+  digitalWrite(motor2b, HIGH);
+  digitalWrite(spd1, HIGH);
+  digitalWrite(spd2, HIGH);
 }
 void left()
 {
-  
-   digitalWrite(motor1f,HIGH);
-  digitalWrite(motor1b,LOW);
- digitalWrite(motor2f,LOW); 
-  digitalWrite(motor2b,HIGH); 
-   digitalWrite(spd1,HIGH);
-  digitalWrite(spd2,HIGH);
-  
+  digitalWrite (motor1b, HIGH);
+  digitalWrite (motor1f, LOW);
+  digitalWrite (motor2b, LOW);
+  digitalWrite (motor2f, HIGH);
+  digitalWrite(spd1, HIGH);
+  digitalWrite(spd2, HIGH);
 }
 void backward()
-{  digitalWrite(motor1f,LOW);
-  digitalWrite(motor1b,HIGH);
-  digitalWrite(motor2f,LOW);
-  digitalWrite(motor2b,HIGH);
-  digitalWrite(spd1,HIGH);
-  digitalWrite(spd2,HIGH);
-}
-void stop()
 {
-   digitalWrite(motor1f,LOW);
-  digitalWrite(motor1b,LOW);
-  digitalWrite(motor2f,LOW);
-  digitalWrite(motor2b,LOW);
-   digitalWrite(spd1,HIGH);
-  digitalWrite(spd2,HIGH);
+  digitalWrite (motor1b, HIGH);
+  digitalWrite (motor1f, LOW);
+  digitalWrite (motor2b, HIGH);
+  digitalWrite (motor2f, LOW);
+  digitalWrite(spd1, HIGH);
+  digitalWrite(spd2, HIGH);
 }
-long usReading()
+void STOP()
 {
-  digitalWrite(5,0);
-delayMicroseconds(2);
-digitalWrite(5,1);
-delayMicroseconds(10);
-digitalWrite(5,0);
+  digitalWrite (motor1b, LOW);
+  digitalWrite (motor1f, LOW);
+  digitalWrite (motor2b, LOW);
+  digitalWrite (motor2f, LOW);
+  digitalWrite(spd1, HIGH);
+  digitalWrite(spd2, HIGH);
+}
+
+long USReadings()
+{
+  digitalWrite(5,LOW);
+  delayMicroseconds(2);
+  digitalWrite(5,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(5,LOW);
   duration = pulseIn(2,HIGH);
-  d= (duration/2)*0.034 ; 
-  return d;
+  distance= (duration/2)*0.034 ; 
+  return distance ;
 }
