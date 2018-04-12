1ae31df5e5627b469a5dee9e6d741f1fb2cc08d0
MonaEltokhy <36299087+MonaEltokhy@users.noreply.github.com>
2018-04-12 16:17:50 +0200

    Add files via upload

---
+++ Line follower/test_2_sensor.ino
@@ -0,0 +1,54 @@
+int motor1f =13;
+int motor1b=12;
+int motor2f=11;
+int motor2b =10;
+
+int LS=4;            //dah ell fe eh sensor aslan
+int CS=7;
+int RS=3;
+
+
+void setup()
+{
+  pinMode(LS, INPUT);
+   pinMode(CS, INPUT); 
+  pinMode(RS, INPUT);
+  pinMode (motor1f, OUTPUT);
+  pinMode (motor1b, OUTPUT);
+  pinMode (motor2f, OUTPUT);
+  pinMode (motor2b, OUTPUT);
+
+}
+void loop() {
+  
+
+  if(digitalRead(RS)==0 && digitalRead(CS)==0 )     // right
+  {
+      digitalWrite (motor1f,HIGH);
+     digitalWrite (motor1b,LOW);
+     digitalWrite (motor2f,HIGH);
+     digitalWrite (motor2b,LOW);
+
+  }
+  else  if(digitalRead(RS)==0 && digitalRead(CS)==1 )     // Move right
+  {
+      digitalWrite (motor1f,HIGH);
+     digitalWrite (motor1b,LOW);
+     digitalWrite (motor2f,HIGH);
+     digitalWrite (motor2b,LOW);
+
+  }
+  else if(digitalRead(RS)==1 && digitalRead(CS)==1)     // Move Forward
+  {
+ digitalWrite (motor1f,HIGH);
+     digitalWrite (motor1b,LOW);
+     digitalWrite (motor2f,HIGH);
+     digitalWrite (motor2b,LOW);
+ }
+   else if(digitalRead(RS)==1 && digitalRead(CS)==0 )     // Move Left
+  {  digitalWrite (motor1f,HIGH);
+     digitalWrite (motor1b,LOW);
+     digitalWrite (motor2f,HIGH); 
+     digitalWrite (motor2b,LOW);
+  }
+}
