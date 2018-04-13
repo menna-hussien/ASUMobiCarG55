3b78255595e08bd213bda1892b66851e1f631fb5
MonaEltokhy <36299087+MonaEltokhy@users.noreply.github.com>
2018-04-14 00:46:34 +0200

    Update test_2_sensor.ino

--- Line follower/test_2_sensor.ino
+++ Line follower/test_2_sensor.ino
@@ -3,13 +3,15 @@ int motor1b=12;
 int motor2f=11;
 int motor2b =10;
 
-int LS=4;            //dah ell fe eh sensor aslan
+int LS=4;            //dah ell fe eh sensor 
 int CS=7;
 int RS=3;
-
+int spd1=7;
+int spd2=6;
 
 void setup()
 {
+  
   pinMode(LS, INPUT);
    pinMode(CS, INPUT); 
   pinMode(RS, INPUT);
@@ -21,6 +23,8 @@ void setup()
 }
 void loop() {
   
+analogWrite(spd1,100);
+analogWrite(spd2,100);
 
   if(digitalRead(RS)==0 && digitalRead(CS)==0 )     // right
   {
