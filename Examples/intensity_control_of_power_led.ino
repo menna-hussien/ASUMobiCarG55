9e882a0f30941c635ae0147c731d4c9992a6d648
AsmaaMohamed12 <36335920+AsmaaMohamed12@users.noreply.github.com>
2018-07-04 02:42:51 +0200

    Add files via upload

---
+++ Examples/intensity_control_of_power_led.ino
@@ -0,0 +1,35 @@
+int pwmPin = 2; // assigns pin 12 to variable pwm
+int pot = A0; // assigns analog input A0 to variable pot
+int c1 = 0;   // declares variable c1
+int c2 = 0;   // declares variable c2
+
+void setup()  // setup loop
+{
+  pinMode(pwmPin, OUTPUT); 
+  pinMode(pot, INPUT);  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+  int value = analogRead(pot);
+  Serial.println(value);
+  c1= value; 
+  c2= 500-c1;         // subtracts c2 from 1000 ans saves the result in c1
+  
+  if (value < 500)
+  {
+  digitalWrite(pwmPin, HIGH); 
+  delayMicroseconds(c2);   
+  digitalWrite(pwmPin, LOW);  
+  delayMicroseconds(c1);   
+  }
+
+  if (value > 500)
+  {
+    digitalWrite(2,LOW);
+  }
+  
+}
+
