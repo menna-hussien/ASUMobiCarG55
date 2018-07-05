2a13a13c43d79c6b5d32f0e1a3c6e626da0d1902
menna-hussien <31513435+menna-hussien@users.noreply.github.com>
2018-07-06 00:17:40 +0200

    detecting smoke
    detecting smoke

---
+++ bonus sensor/smoke_sensor.ino
@@ -0,0 +1,25 @@
+
+
+ 
+/*Code for MQ-2 Smoke Sensor Circuit Built with an Arduino Board*/
+
+const int sensorPin= 0;
+const int buzzerPin= 13;
+int smoke_level;
+
+void setup() {
+Serial.begin(115200); //sets the baud rate for data transfer in bits/second
+pinMode(sensorPin, INPUT);//the smoke sensor will be an input to the arduino
+pinMode(buzzerPin, OUTPUT);//the buzzer serves an output in the circuit
+}
+
+void loop() {
+smoke_level= analogRead(sensorPin); //arduino reads the value from the smoke sensor
+Serial.println(smoke_level);//prints just for debugging purposes, to see what values the sensor is picking up
+if(smoke_level > 200){ //if smoke level is greater than 200, the buzzer will go off
+digitalWrite(buzzerPin, HIGH);
+}
+else{
+digitalWrite(buzzerPin, LOW);
+}
+}
